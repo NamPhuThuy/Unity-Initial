056eeb23c32cd3df6138e84bfc92884c16c89b81
NamPhuThuy <ttnam957@gmail.com>
2025-10-18 17:27:09 +0700

    modify: remove scripts

--- Assets/Scripts/Const/LayerConst.cs
+++
@@ -1,9 +0,0 @@
-namespace NamPhuThuy.Common
-{
-    public static class LayerConst
-    {
-        public const string DEFAULT = "Default";
-        public const string GROUND = "Ground";
-        public const string IGNORECAMERA = "IgnoreCamera";
-    }
-}
